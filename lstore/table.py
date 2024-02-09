2c29f09ede975473c71f95669b170c648cf5c5d9
syoo008 <71057809+syoo008@users.noreply.github.com>
2024-02-08 22:30:24 -0800

    Update table.py
    Tried to implement a page_directory and a get_page_range function that returns the id of the page_range desired

--- lstore/table.py
+++ lstore/table.py
@@ -2,6 +2,8 @@ from lstore.index import Index
 from lstore.index import Page #added Page because pdf says Table uses Page internally
 from time import time
 
+import numpy as np
+
 INDIRECTION_COLUMN = 0
 RID_COLUMN = 1
 TIMESTAMP_COLUMN = 2
@@ -26,14 +28,14 @@ class Table:
         self.name = name
         self.key = key
         self.num_columns = num_columns
-        self.page_directory = {}
+        self.page_directory = np.array([])
         self.index = Index(self)
         self.num_records = 0
 
-        page_range = PageRange(num_columns) 
+        add_page_range(self, num_columns)
         pass
     
-    # If page_directory holds IDs of page ranges...
+    # If page_directory holds IDs of page ranges...  
     # Possible implementation of page_directory -> RID
     # Not sure how to assign an ID to tail pages or if that is even needed
     # Would need to make sure tail page ID can't be the same as any 
@@ -44,20 +46,30 @@ class Table:
     def add_page_range(self, numCols):
         page_range = PageRange(numCols) 
         #page_range.id = #can make it equal to the value of the last index in page_directory + 1
-        self.page_directory.append(page_range.id)
+        page_range.id = len(self.page_directory) * 64000 #1. 0 2. 64000 3. 128000 etc
+        self.page_directory = np.append(self.page_directory([page_range.id]))
         num_records += MAX_RECORDS_PER_PAGE_RANGE
         
-    def get_page_range(self, rid)
+    def get_page_range(self, rid):
         # Maybe a tree or hash table for the page_directory?
-        #
-        return page_range
+        #could implement a faster search alrgorithm than just going through page_directory one by one
+        for i in len(self.page_directory) - 1:
+            if rid > self.page_directory[i] and rid < self.page_directory[i + 1]:
+                correct_page_range_id = self.page_directory[i] #can get the id of the page_range containing the rid but how do i acually return the right page range (where are page_ranges stored and how do i access ones already made?)
+        return correct_page_range_id
+
+    def get_page(self, page_range_id, rid):
+        #idk how to access pages or records from here
+        pass
         
+    
     def find_record(self, rid):
         # This seems inefficient so maybe there is a better way
         # If this works, need to add these 3 methods:
-        page_range = get_page_range(rid)
-        page = get_page(page_range, rid)
-        record = get_record(page)
+        page_range_id = get_page_range(rid)
+        page = get_page(page_range_id, rid)
+        record = get_record(page, rid)
+        pass
         
 
     def __merge(self):
