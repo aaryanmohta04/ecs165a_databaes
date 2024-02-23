feef40b6d7607110a1c9362ed6de3093215d82c0
Michael Loomis <63444134+michaelloomis@users.noreply.github.com>
2024-02-22 21:27:59 -0800

    Started bufferpool

--- lstore/bufferpool.py
+++ lstore/bufferpool.py
@@ -14,5 +14,34 @@ class Bufferpool:
     def has_capacity(self):
         if self.numFrames == FRAMECOUNT:
             return True
-        
         return false
+    
+    #Select LRU(or whatever method), send to disk
+    def evict_page(self):
+        
+    #Bring page in, if full, evict a page first
+    def load_page(self):
+        
+    #Write all pages to disk
+    def close(self):
+        
+    
+class Frame:
+
+    def __init__(self, path):
+        
+        self.dirtyBit = False
+        self.pin = False
+        self.path = path
+        
+    def set_dirty_bit(self):
+        if dirtyBit == True:
+            dirtyBit = False
+        else:
+            dirtyBit = True
+        
+    def set_pit(self):
+        if pin == True:
+            pin = False
+        else:
+            pin = True
