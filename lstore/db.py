61d6a0e6a708c74355112c66c91534cb6d7cc591
syoo008 <71057809+syoo008@users.noreply.github.com>
2024-02-27 14:53:11 -0800

    addded some bufferpool capabilities

--- lstore/db.py
+++ lstore/db.py
@@ -1,13 +1,18 @@
 from lstore.table import Table
+from lstore.bufferpool import *
 
 class Database():
 
     def __init__(self):
         self.tables = []
+        #self.tableDirectory = []
+        self.bufferpool = None
         pass
 
     # Not required for milestone1
     def open(self, path):
+        self.bufferpool = Bufferpool(path) #create a bufferpool object for this db
+        self.bufferpool.start_dir() #initiate directory given the path (or check if it exists)
         pass
 
     def close(self):
@@ -20,8 +25,11 @@ class Database():
     :param key: int             #Index of table key in columns
     """
     def create_table(self, name, num_columns, key_index):
-        table = Table(name, num_columns, key_index)
+        self.bufferpool.start_table_dir(name, num_columns) #makes table directory
+        self.bufferpool.allocate_page_range(self, num_columns, 0)
+        table = Table(name, num_columns, key_index, self.bufferpool)
         self.tables.append(table)
+
         for table in self.tables:
             print(table.name)
         return table
@@ -46,4 +54,6 @@ class Database():
             if table.name == name: 
                 return table 
         print(self.tables)
+
+        self.bufferpool.get_table_dir(name)
         pass
