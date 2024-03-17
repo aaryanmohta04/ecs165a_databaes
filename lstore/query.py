721994c86883b3a0aee08bf6e25d3205a34b73ca
Michael Loomis <63444134+michaelloomis@users.noreply.github.com>
2024-03-16 23:00:48 -0700

    Index

--- lstore/query.py
+++ lstore/query.py
@@ -61,25 +61,39 @@ class Query:
     # Assume that select will never be called on a key that doesn't exist
     """
     def select(self, search_key, search_key_index, projected_columns_index):
-        # if self.table.index.has_index(search_key_index) == False:
-        #     self.table.index.create_index(search_key_index)
+        if self.table.index.indices[search_key_index] == None:
+            # find rids without the index
+            pass
         
-        rid = self.table.index.locate(search_key_index, search_key)
+        rids = self.table.index.locate(search_key_index, search_key)
         records = []
-        # for rid in rids:
-        # rid = self.table.page_directory[rid]
-        frame_index = self.table.bufferpool.load_base_page(rid[0], rid[1], self.table.num_columns, self.table.name)
-        newrid = []
-        key_directory = (rid[0], rid[1], 'b')
-        newrid = self.table.bufferpool.frames[frame_index].indirection[rid[2]]
-        if (newrid == [0,0,0,'d']):
-            print("error, tried selecting deleted record")
-        rid = newrid
-        # TPS = (self.table.bufferpool.frames[frame_index].TPS[rid[2]])
-        # print(TPS)
-        TPS = [0,0]
-        record = self.table.find_record(search_key, rid, projected_columns_index, TPS)
-        records.append(record)
+        if isinstance(rids, tuple):
+            rid = rids
+            rid = self.table.page_directory[rid]
+            frame_index = self.table.bufferpool.load_base_page(rid[0], rid[1], self.table.num_columns, self.table.name)
+            newrid = []
+            key_directory = (rid[0], rid[1], 'b')
+            newrid = self.table.bufferpool.frames[frame_index].indirection[rid[2]]
+            if (newrid == [0,0,0,'d']):
+                print("error, tried selecting deleted record")
+            rid = newrid
+            TPS = [0,0]
+            record = self.table.find_record(search_key, rid, projected_columns_index, TPS)
+            records.append(record)
+            return records
+            
+        for rid in rids:
+            rid = self.table.page_directory[rid]
+            frame_index = self.table.bufferpool.load_base_page(rid[0], rid[1], self.table.num_columns, self.table.name)
+            newrid = []
+            key_directory = (rid[0], rid[1], 'b')
+            newrid = self.table.bufferpool.frames[frame_index].indirection[rid[2]]
+            if (newrid == [0,0,0,'d']):
+                print("error, tried selecting deleted record")
+            rid = newrid
+            TPS = [0,0]
+            record = self.table.find_record(search_key, rid, projected_columns_index, TPS)
+            records.append(record)
         return records
         pass
     
@@ -124,10 +138,15 @@ class Query:
     # Returns False if no records exist with given key or if the target record cannot be accessed due to 2PL locking
     """
     def update(self, primary_key, *columns):
-        rid = self.table.index.locate(self.table.key, primary_key) #gets rid using key in index
-        BaseRID = rid #sets baseRID to the rid found with key
-        #oldRID = rid looks like it's the same as currentRID? so probably not needed
-        rid = self.table.page_directory[rid] #sets rid to the physical location of record using page_directory
+        rid = self.table.index.locate(self.table.key, primary_key)
+        
+        #Add values in columns to each index
+        for i in len(columns):
+            self.table.index.indices[i][columns[i]] = rid
+            #Need to delete the key:rid prior to update
+            
+        BaseRID = rid
+        rid = self.table.page_directory[rid]
         self.table.updateRec(rid, BaseRID, primary_key, *columns)
 
         pass
